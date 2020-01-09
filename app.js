7652bc11a145de9ba12679423efa467561ed8c73
kiranbalampaki <kiranmgr1996@gmail.com>
2020-01-09 19:26:59 +0545

    routes added

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        test: 'Everything Okay'
-    });
-});
+const productRoutes = require("./api/routes/products");
+
+app.use('/products', productRoutes);
+
 
 module.exports = app;
